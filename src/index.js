9f0d969a93a01d1e0dabe67739687c9102f95b96
Tyler Durden <tylerdurden1412@gmail.com>
2022-06-26 19:46:00 +0300

    add delete btn script

--- src/index.js
+++ src/index.js
@@ -5,13 +5,82 @@ const doneBtn = document.querySelector('[data-done-button]')
 const addListInput = document.querySelector('[data-list-input]')
 const projects = document.querySelector('[data-my-projects]')
 
+const deleteBtn = document.querySelector('[data-delete-button]')
 
-var myProjects = new Array();
-myProjects
 
+
+
+var myProjects = [{name:'today',selected:true}];
+
+
+function deleteProject() {
+    deleteBtn.addEventListener('click',() => {
+        let selected = document.querySelector('.selectedTask');
+        selected.parentNode.removeChild(selected)
+
+        console.log(selected.innerHTML)
+
+        myProjects.forEach(project => {
+            if (project.name === selected.innerHTML) {
+                project.name = '';
+            }
+        })
+    })
+}
+
+function makeSelectedCss() {
+    projects.addEventListener('click',(e) => {
+        if (document.querySelector('.selectedTask') === null) {
+            projects.firstElementChild.classList.add('selectedTask')
+            return
+        }
+
+        if (e.target.classList[0] === 'myTasksList') {
+            return
+        }
+        var prevSelected = document.querySelector('.selectedTask')
+        prevSelected.classList.remove('selectedTask')
+        e.target.classList.add('selectedTask')
+
+        var selected = document.querySelector('.selectedTask');
+        changeObjectSelected(selected.innerHTML,prevSelected.innerHTML);
+    })
+    
+}
+
+function clearArray(arr) {
+    let newMyProjects = arr.filter(function (e) {
+        return e.name = '';
+    })
+
+    console.log(newMyProjects)
+}
+
+function checkEmpty(word) {
+    if (word === '') {
+        return true
+    }
+}
+
+function makeSelected() {
+    myProjects.forEach(project => {
+        if (project.selected === true) {
+            let selectedName = project.name;
+        }
+    })
+
+    let lis = projects.getElementsByTagName('li')
+    let lisArr = Array.prototype.slice.call(lis);
+    lisArr.forEach(li => {
+        if (selectedName === li.innerHTML) {
+            projects.getEle
+        }
+    })
+}
 
 function createProjectObject(name) {
     this.name = name;
+    this.selected = false;
 }
 
 editTitle.addEventListener('click',() => {
@@ -62,13 +131,34 @@ function displayProjects(myProjects) {
     });
 }
 
+function changeObjectSelected(selection,previousSelection) {
+    myProjects.forEach(project => {
+        if (project.name === selection) {
+            project.selected = true;
+        }
+        if (project.name === previousSelection) {
+            project.selected = false;
+        }
+    })
+}
+
 addListInput.addEventListener('keydown',(e) => {
+    
     if (e.key === 'Enter') {
-        addProject(addListInput.value)
-        let projectObject = new createProjectObject(addListInput.value)
-        addListInput.value = ''
-        myProjects.push(projectObject);
-        console.log(myProjects)
+        let checkIn = checkIfNameExists(addListInput.value);
+        if (checkIn) {
+            addListInput.value = ''
+            return
+        }
+        else {
+            addProject(addListInput.value)
+            let namopoulos = addListInput.value
+            namopoulos = namopoulos.toLowerCase()
+            let projectObject = new createProjectObject(namopoulos)
+            addListInput.value = ''
+            myProjects.push(projectObject);
+            console.log(myProjects)
+        }
     }
     
 })
@@ -78,6 +168,7 @@ function addProject(listName) {
 
 
     let projectLi = document.createElement('li');
+    projectLi.id = listName
 
     projectLi.innerHTML = listName;
 
@@ -87,5 +178,31 @@ function addProject(listName) {
 function addProjectToMyProjects(projectName) {
     
 }
+function checkIfNameExists(namo) {
+    namo = namo.toLowerCase()
+    let check = false
+    myProjects.forEach(project => {
+        if (project.name === namo) {
+            check = true;
+        }
+    })
 
-displayProjects();
+    
+
+    return check
+}
+
+// function checkSelected(myProjects) {
+//     myProjects.forEach(project => {
+//         if (project.selected === true) {
+
+//         }
+//     })
+// }
+
+displayProjects(myProjects);
+makeSelectedCss();
+deleteProject();
+// checkSelected(myProjects);
+// makeSelected()
+projects.firstElementChild.classList.add('selectedTask')
